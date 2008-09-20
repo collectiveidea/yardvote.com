f0f8791526854e17ec4b43724ae90012847ded66
Daniel Morrison <daniel@collectiveidea.com>
2008-09-20 15:57:18 -0400

    Added Yahoo & Google API keys.

---
+++ config/initializers/geo.rb
@@ -0,0 +1,4 @@
+# http://signs.collectiveidea.com
+GOOGLE_MAPS_API_KEY = 'ABQIAAAAKU0JUXXK40nehgHiZGe2JRQSWFFyx9Ag03EiE0CoVWCpvzNSRhQ_o7RDkUypWKJFprXA1a55GhYpHw'
+YAHOO_API_KEY = '16AGnAvV34HZtyfGOV7LAsPG9gwnmA7Sjx24vtcDKKFOoyuLAgkC_VltUvBUUA--'
+Geocode.geocoder = Graticule.service(:yahoo).new YAHOO_API_KEY
