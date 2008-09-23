1c15ace5a6a3415f6b9f94a236ce805b2a37c2e9
Brandon Keepers <brandon@collectiveidea.com>
2008-09-22 23:02:28 -0400

    reset form after submitting

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,7 +2,7 @@ Event.addBehavior({
   'form.new_location:submit': function(event) {
     event.stop();
     this.request({
-      
+      onSuccess: function() { this.reset(); }.bind(this)
     });
   }
   
