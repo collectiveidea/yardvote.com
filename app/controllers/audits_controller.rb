8b99bb3b0ceb110223f9872562b1e7a9887899b3
Daniel Morrison <daniel@collectiveidea.com>
2008-09-29 23:59:51 -0400

    Limit number of audits displayed.

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -1,5 +1,5 @@
 class AuditsController < ApplicationController
   def index
-    @audits = Audit.all(:order => 'created_at DESC')
+    @audits = Audit.all(:order => 'created_at DESC', :limit => 100)
   end
 end
