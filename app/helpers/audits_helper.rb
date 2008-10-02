8a5deafc00b4ff9b05882cb7273481ea51996775
Daniel Morrison <daniel@collectiveidea.com>
2008-10-02 08:29:10 -0400

    Better grammar.

--- app/helpers/audits_helper.rb
+++ app/helpers/audits_helper.rb
@@ -1,2 +1,8 @@
 module AuditsHelper
+  
+  # yes, this is overly simplistic, but I only have a couple use cases
+  # create, update, destroy
+  def past_tense(present)
+    present.sub(/e?$/, 'ed')
+  end
 end
