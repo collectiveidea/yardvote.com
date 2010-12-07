59f8a48b2f00de4b390e0fbb5700d8dc3a517955
Daniel Morrison <daniel@collectiveidea.com>
2010-12-06 20:20:15 -0500

    Converting to Rails 2.3 and bundler.

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source :rubygems
+gem "rails", "2.3.9"
+
+gem 'graticule'
+gem 'acts_as_geocodable'
+gem 'acts_as_audited'
+
+group :development do
+  gem 'sqlite3-ruby', '1.2.5', :require => "sqlite3"
+end
+
+group :test do
+  gem 'rspec-rails',  '1.3.2', :require => false
+end
---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '5204ce8bc7a677178c88098f336c5dd8744e54cc0c5ffeda22eb9c322f97cce42371456378817e8cc6ec9becdce4149d17246eca77a6e0e46acec889f7f0d642';
